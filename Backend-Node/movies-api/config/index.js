53693948c31b8e4ebe4c3f107a7e49a018ab8dac
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-11-05 15:16:06 -0300

    Connection mongodb vanilla

--- Backend-Node/movies-api/config/index.js
+++ Backend-Node/movies-api/config/index.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const config  = {
     dev: process.env.NODE_ENV !== 'production',
     port: process.env.PORT || 3000,
+    cors: process.env.env.CORS,
+    dbUser: process.env.env.DB_USER,
+    dbPassword: process.env.env.DB_PASSWORD,
+    dbHost: process.env.env.DB_HOST,
+    dbName: process.env.env.DB_NAME
 }
 
 module.exports = { config }
---
+++ Backend-Node/movies-api/lib/mongo.js
@@ -0,0 +1,34 @@
+const {MongoClient} = require('mongodb');
+const {config} = require('../config/index')
+
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const DB_NAME = config.dbName;
+
+const MONGO_URI = `mongodb+srv://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${DB_NAME}?retryWrite=true&w=majority`
+
+class MongoLib {
+    constructor(){
+        this.client = new MongoClient(MONGO_URI,{ useNewUrlParser: true})
+        this.dbName = DB_NAME
+    } 
+
+
+    connect(){
+        if(!MongoLib.connect){
+            MongoLib.connection = new Promise((resolve, reject)=>{
+                this.client.connect(err=>{
+                    if(err){
+                        reject(err)
+                    }
+                    console.log('Connected succesfully to mongo');
+                    resolve(this.client.db(this.dbName))
+                })
+            })
+        }
+
+        return MongoLib.connection
+    }
+}
+
+module.exports = MongoLib;
