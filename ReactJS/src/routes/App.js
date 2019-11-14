719e36ca67efd33fdf3a2c3186a19b2a0f504c1c
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-11-14 10:16:07 -0300

    Add page Login

--- ReactJS/src/routes/App.js
+++ ReactJS/src/routes/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter,Switch, Route } from 'react-router-dom'
 import Home from '../containers/Home'
+import {Login} from '../containers/Login'
 
 const App = ()=>(
         
         <BrowserRouter>
-            <Route exact path="/" component={Home} />
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+            </Switch>
         </BrowserRouter>
 
 )
