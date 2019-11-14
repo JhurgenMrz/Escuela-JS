074750e8850875fb203fdd232ebc374c688bc6ac
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-11-14 14:51:12 -0300

    Add funtion of gravatar for ImageUsers

--- ReactJS/src/components/Header.jsx
+++ ReactJS/src/components/Header.jsx
@@ -1,37 +1,52 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {gravatar} from '../utils/gravatar'
 import '../assets/styles/components/Header.scss'
 import logo from '../assets/static/logo-platzi-video-BW2.png'
 import userIcon from '../assets/static/user-icon.png'
 
 
-export const Header = ()=>(
+const Header = props =>{
+
+  const { user } = props
+
+  const hasUser = Object.keys(user).length > 0
+
+  return (
     <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt=""/>
-        <p>Perfil</p>
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {
+            hasUser ? 
+              <img src={gravatar(user.email)} alt={user.email} />
+            : 
+              <img src={userIcon} alt="" />
+            }
+          <p>Perfil</p>
+        </div>
+        <ul>
+          <li>
+            <Link to="/">Cuenta</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to="/">
-            Cuenta
-          </Link>
-        </li>
-        <li>
-          <Link to="/login">
-            Login
-          </Link>
-        </li>
-        <li>
-          <Link to="/register">
-            Register
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-)
+    </header>
+  );}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps, null)(Header)
---
+++ ReactJS/src/utils/gravatar.js
@@ -0,0 +1,10 @@
+import md5 from 'md5';
+
+export const gravatar = (email) => {
+    const base = 'https://gravatar.com/avatar/';
+    console.log(email);
+    const formattedEmail = (email).trim().toLowerCase()
+    const hash = md5(formattedEmail, { encoding: 'binary' })
+
+    return `${base}${hash}`
+}
