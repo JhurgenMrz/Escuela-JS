9bc5ff1f20824c347fcc99f4847fde097557ddab
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-11-17 17:17:26 -0300

    Initial course Auth with PassportJs

---
+++ Auth-PassportJs/jwt-utilities/index.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const [, ,option, secret, nameOrToken] = process.argv;
+
+if( !option || !secret || !nameOrToken){
+    return console.log('Missing arguments');
+}
+
+function signToken(payload, secret){
+    return jwt.sign(payload, secret)
+}
+
+function verityToken(token,secret){
+    return jwt.verify(token, secret)
+
+}
+
+if(option === 'sign'){
+    console.log(signToken({sub: nameOrToken}, secret));
+}else if(option === 'verify'){
+    console.log(verityToken(nameOrToken, secret));
+}else{
+    console.log('Options needs to be "sign" or "verify" ');
+}
