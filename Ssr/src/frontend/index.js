cb197e98accfd702fb1277b88a0452803c058b2b
Jhurgen Maraza <JhurgenMrz99@gmail.com>
2019-12-03 14:14:33 -0300

    Initial BFF

--- Ssr/src/frontend/index.js
+++ Ssr/src/frontend/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk'
 import { createBrowserHistory } from 'history';
 import { Router } from 'react-router';
 import reducer from './reducers';
+import { createBrowserHistory } from 'history'
 
 import App from './routes/App';
 
@@ -13,14 +15,14 @@ if (typeof window !== 'undefined') {
   if (process.env.NODE_ENV === 'production') composeEnhancers = compose;
   else composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const preloadedState = window.__PRELOADED_STATE__;
-  const store = createStore(reducer, preloadedState, composeEnhancers());
+  const store = createStore(reducer, preloadedState, composeEnhancers(applyMiddleware(ReduxThunk)));
 
   const history = createBrowserHistory();
 
   hydrate(
     <Provider store={store}>
       <Router history={history}>
-        <App />
+        <App isLogged={(preloadedState.use.id)}/>
       </Router>
     </Provider>,
     document.getElementById('app'),
--- Ssr/src/server/server.js
+++ Ssr/src/server/server.js
@@ -1,49 +1,100 @@
-import express from 'express';
-import path from 'path';
-import dotenv from 'dotenv'
-import webpack from 'webpack'
-import main from './routes/main'
-import helmet from 'helmet'
+import express from "express";
+import dotenv from "dotenv";
+import webpack from "webpack";
+import main from "./routes/main";
+import helmet from "helmet";
+import axios from "axios";
+import passport from "passport";
+import boom from "@hapi/boom";
+import cookieParser from "cookie-parser";
+import { config } from "../../config";
 
 dotenv.config();
 
 const ENV = process.env.NODE_ENV;
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-app.use(express.static(`${__dirname}/public`))
 
-if(ENV === 'development'){
-    console.log('Loading dev config');
-    const webpackConfig = require('../../webpack.config');
-    const webpackDevMiddleware = require('webpack-dev-middleware');
-    const webpackHotMiddleware = require('webpack-hot-middleware');
-    const compiler = webpack(webpackConfig);
-    const serverConfig = {
-        contentBase: `http://localhost:${PORT}`,
-        port: PORT,
-        publicPath: webpackConfig.output.publicPath,
-        hot: true,
-        historyApiFallback: true,
-        stats: {
-            colors: true
-        },
-    };
+//Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
-    app.use(webpackDevMiddleware(compiler, serverConfig));
-    app.use(webpackHotMiddleware(compiler))
-} else{
-    app.use(helmet());
-    app.use(helmet.permittedCrossDomainPolicies());
-    app.disable('x-powered-by');
+//Basic Strategy
+require("./utils/auth/strategies/basic");
+
+app.use(express.static(`${__dirname}/public`));
+
+if (ENV === "development") {
+  console.log("Loading dev config");
+  const webpackConfig = require("../../webpack.config");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
+  const compiler = webpack(webpackConfig);
+  const serverConfig = {
+    contentBase: `http://localhost:${PORT}`,
+    port: PORT,
+    publicPath: webpackConfig.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    stats: {
+      colors: true
+    }
+  };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+} else {
+  app.use(helmet());
+  app.use(helmet.permittedCrossDomainPolicies());
+  app.disable("x-powered-by");
 }
 
+app.post("/auth/sign-in", async function(req, res, next) {
+  passport.authenticate("basic", function(error, data) {
+    try {
+      if (error || !data) {
+        next(boom.unauthorized());
+      }
+      req.login(data, { session: false }, async function(error) {
+        if (error) {
+          next(error);
+        }
+        const { token, ...user } = data;
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev
+        });
 
-app.get('*', main)
-
-app.listen(PORT, (err)=>{
-    if(err){
-        console.log(err);
+        res.status(200).json(user);
+      });
+    } catch (error) {
+      next(error);
     }
-    console.log(`Server on PORT http://localhost:${PORT}`);
-})
+  })(req, res, next);
+});
+
+app.post("/auth/sign-up", async function(req, res, next) {
+  const { body: user } = req;
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    });
+
+    res.status(201).json({ message: "user created" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("*", main);
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Server on PORT http://localhost:${PORT}`);
+});
